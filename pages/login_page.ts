1e80a3b74f2d0bdb91aa3bf65d22f43afb46921a
ElenBerhane12 <148052480+ElenBerhane12@users.noreply.github.com>
2024-09-24 00:35:58 +0200

    Update login_page.ts

--- pages/login_page.ts
+++ pages/login_page.ts
@@ -3,32 +3,30 @@ import { Locator, Page } from '@playwright/test';
 export class LoginPage {
     // Attributes/Locators
     readonly page: Page;
-    readonly usernameTF: Locator; // Text field for username
-    readonly passwordTF: Locator; // Text field for password
-    readonly loginBTN: Locator;   // Button to perform login
+    readonly usernameTF: Locator; 
+    readonly passwordTF: Locator; 
+    readonly loginBTN: Locator;   
 
     // Constructor
     constructor(page: Page) {
         this.page = page;
         
         // Initialize the locators
-        this.usernameTF = page.locator('input[name="username"]'); // Adjust the selector as needed
-        this.passwordTF = page.locator('input[name="password"]'); // Adjust the selector as needed
-        this.loginBTN = page.locator('button[type="submit"]');    // Adjust the selector as needed
+        this.usernameTF = page.locator('input[name="username"]'); 
+        this.passwordTF = page.locator('input[name="password"]'); 
+        this.loginBTN = page.locator('button[type="submit"]');    
     }
 
-    // Methods
 
-    // Method to perform login
+
     async performLogin(username: string, password: string) {
-        // Fill in the username and password fields
+        
         await this.usernameTF.fill(username);
         await this.passwordTF.fill(password);
         
-        // Click the login button
+        
         await this.loginBTN.click();
         
-        // Wait for the page to load after login
-        await this.page.waitForLoadState('networkidle'); // Adjust as needed based on your application
+        await this.page.waitForLoadState('networkidle'); 
     }
 }
